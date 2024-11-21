a18f40da5bf291da79790c98d768c3df0898f0df
vthang0412 <vthang041206@gmail.com>
2024-11-21 23:13:15 +0700

    Update ss3-ex5.cpp

--- ss3-ex5.cpp
+++ ss3-ex5.cpp
@@ -7,7 +7,7 @@ int main() {
 	printf("|2   | Nguyen Van B | 21   | 0904488482 | bnv@rikkeiacademy.com  |\n");
 	printf("|3   | Nguyen Van C | 18   | 0904488483 | cnv@rikkeiacademy.com  |\n");
 	printf("|4   | Nguyen Van D | 19   | 0904488484 | dnv@rikkeiacademy.com  |\n");
-    printf("|5   | Nguyen Van E | 22   | 0904488485 | env@rikkeiacademy.com  |\n");
+        printf("|5   | Nguyen Van E | 22   | 0904488485 | env@rikkeiacademy.com  |\n");
 	printf("|6   | Nguyen Van F | 21   | 0904488486 | fnv@rikkeiacademy.com  |\n");
 	printf("|7   | Nguyen Van G | 23   | 0904488487 | gnv@rikkeiacademy.com  |\n");
 	printf("|8   | Nguyen Van H | 19   | 0904488488 | hnv@rikkeiacademy.com  |\n");
