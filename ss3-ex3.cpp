1feb7bb953f2c1506b2247f0d57b2d73c3677043
vthang0412 <vthang041206@gmail.com>
2024-11-22 00:21:29 +0700

    Update ss3-ex3.cpp

--- ss3-ex3.cpp
+++ ss3-ex3.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h> 
 int main (){
-	float pi=3.14,r=5 ,chuvi, dientich;
+	float pi=3.14,r,chuvi, dientich;
+	printf("Nhap ban kinh hinh tron: ");
+        scanf("%f", &r);
 	chuvi =2*pi*r;
 	dientich=2* r*r;
 	printf ("chu vi hinh tron la : %.2f\n ",chuvi) ;
