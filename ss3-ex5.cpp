322bca49d4eb14fcfe01f918aad77d11a54b7e9d
vthang0412 <vthang041206@gmail.com>
2024-11-22 00:49:38 +0700

    Update ss3-ex5.cpp

--- ss3-ex5.cpp
+++ ss3-ex5.cpp
@@ -1,17 +1,30 @@
 #include<stdio.h>
 
 int main() {
+	printf("------------------------------------------------------------------\n");
 	printf("|                      DANH SACH SINH VIEN                       |\n");
+	printf("------------------------------------------------------------------\n");
 	printf("|STT | Ho va ten    | Tuoi | SDT        | Email                  |\n");
+	printf("------------------------------------------------------------------\n");
 	printf("|1   | Nguyen Van A | 20   | 0904488481 | anv@rikkeiacademy.com  |\n");
+	printf("------------------------------------------------------------------\n");
 	printf("|2   | Nguyen Van B | 21   | 0904488482 | bnv@rikkeiacademy.com  |\n");
+	printf("------------------------------------------------------------------\n");
 	printf("|3   | Nguyen Van C | 18   | 0904488483 | cnv@rikkeiacademy.com  |\n");
+	printf("------------------------------------------------------------------\n");
 	printf("|4   | Nguyen Van D | 19   | 0904488484 | dnv@rikkeiacademy.com  |\n");
+	printf("------------------------------------------------------------------\n");
         printf("|5   | Nguyen Van E | 22   | 0904488485 | env@rikkeiacademy.com  |\n");
+        printf("------------------------------------------------------------------\n");
 	printf("|6   | Nguyen Van F | 21   | 0904488486 | fnv@rikkeiacademy.com  |\n");
+	printf("------------------------------------------------------------------\n");
 	printf("|7   | Nguyen Van G | 23   | 0904488487 | gnv@rikkeiacademy.com  |\n");
+	printf("------------------------------------------------------------------\n");
 	printf("|8   | Nguyen Van H | 19   | 0904488488 | hnv@rikkeiacademy.com  |\n");
+	printf("------------------------------------------------------------------\n");
 	printf("|9   | Nguyen Van I | 18   | 0904488489 | inv@rikkeiacademy.com  |\n");
-	printf("|10  | Nguyen Van K | 21   | 0904488480 | knv@rikkeiacademy.com  |");
+	printf("------------------------------------------------------------------\n");
+	printf("|10  | Nguyen Van K | 21   | 0904488480 | knv@rikkeiacademy.com  |\n");
+	printf("------------------------------------------------------------------\n");
 	return 0;
 }
