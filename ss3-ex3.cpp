cd168997fd353c758432fd010b56105334bdba5a
vthang0412 <vthang041206@gmail.com>
2024-11-21 23:12:10 +0700

    Add files via upload

---
+++ ss3-ex3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main (){
+	float pi=3.14,r=5 ,chuvi, dientich;
+	chuvi =2*pi*r;
+	dientich=2* r*r;
+	printf ("chu vi hinh tron la : %.2f\n ",chuvi) ;
+	printf ("dien tich hinh tron la : %.2f\n ",dientich) ;
+	return 0; 
+} 
